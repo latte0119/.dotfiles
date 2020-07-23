84866236eb9881eec0144c75bbcf6b31d42d0278
latte0119 <latte.malta@gmail.com>
2020-07-23 13:39:22 +0900

    fix init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ DIR=`pwd`
 
 cd ~
 ln -s -f ${DIR}/.bashrc .bashrc
-ln -s -f ${DIR}.profile .profile
+ln -s -f ${DIR}/.profile .profile
 ln -s -f ${DIR}/.gitconfig .gitconfig
 ln -s -f ${DIR}/.vimrc .vimrc
 
