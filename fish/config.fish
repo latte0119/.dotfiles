767c1d290b5ed8dcccd7d55300886007ea1d37a2
latte0119 <latte.malta@gmail.com>
2020-10-08 21:36:17 +0900

    latexmkrc

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
 alias dc='sudo docker-compose'
 alias rmdp='sudo docker rm (sudo docker ps -aq)'
 alias rmdi='sudo docker rmi (sudo docker images -q)'
+alias ll='ls -AlF'
+
--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ ln -s -f ${DIR}/.bashrc .bashrc
 ln -s -f ${DIR}/.profile .profile
 ln -s -f ${DIR}/.gitconfig .gitconfig
 ln -s -f ${DIR}/.vimrc .vimrc
+ln -s -f ${DIR}/.latexmkrc .latexmkrc
 
 rm -rf .vim
 ln -s ${DIR}/.vim .vim
